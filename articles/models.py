83b9d27014faabbf2828b00425ca85a4024837e4
AndreMuderhwa <safariandre66@gmail.com>
2023-10-03 12:02:45 +0200

    slug

--- articles/models.py
+++ articles/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 from django.utils.text import slugify
+from django.db.models.signals import pre_save,post_save
+
 # Create your models here.
 class Article(models.Model):
     title=models.CharField(max_length=120)
@@ -10,5 +12,25 @@ class Article(models.Model):
     updated=models.DateField(null=True,auto_now=True)
     publish=models.DateTimeField(auto_now_add=False,auto_now=False,default=timezone.now)  
 
-    def save(*args, **kwargs):
+    def save(self,*args, **kwargs):
+        # if self.slug is None:
+        #     self.slug=slugify(self.title)
         super().save(*args, **kwargs)
+
+def article_pre_save(sender,instance,*args, **kwargs):
+    print("pre_save")
+    if instance.slug is None:
+            instance.slug=slugify(instance.title)
+   
+
+pre_save.connect(article_pre_save,sender=Article)
+
+
+def article_post_save(sender,instance,created,*args, **kwargs):
+    print("post_save")
+    print(args,kwargs)
+    if created:
+         instance.slug="this is my slug"
+         instance.save()
+
+post_save.connect(article_post_save,sender=Article)
