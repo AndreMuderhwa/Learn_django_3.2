2500c76e8fd6acdda9a527b216a7b661dca12b80
AndreMuderhwa <safariandre66@gmail.com>
2023-10-11 14:57:52 +0200

    go deep test

--- articles/tests.py
+++ articles/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from .models import Article
 from django.utils.text import slugify
+from .utils import slugify_instance_title
 # Create your tests here.
 
 class ArticleTestCase(TestCase):
@@ -23,5 +24,28 @@ class ArticleTestCase(TestCase):
         slug=obj.slug
         slugified_title=slugify(title)
         self.assertEqual(slug,slugified_title)
+
+    def test_hello_world_unique_slug(self):
+        qs=Article.objects.exclude(slug__iexact='hello-world')
+        for obj in qs:
+            title=obj.title
+            slug=obj.slug
+            slugified_title=slugify(title)
+            self.assertEqual(slug,slugified_title)
+
+    def test_slugify_instance_title(self):
+        obj=Article.objects.all().last()
+        new_slugs=[]
+        for i in range(0,5):
+            instance=slugify_instance_title(obj, save=False)
+            new_slugs.append(instance.slug)
+        unique_slug=list(set(new_slugs))
+        self.assertEqual(len(new_slugs),len(unique_slug))
+    
+    def test_slugify_instance_title_redux(self):
+        slug_list=Article.objects.all().values_list('slug',flat=True)
+        unique_slug_list=list(set(slug_list))
+        self.assertEqual(len(slug_list),len(unique_slug_list))
+
 
 
