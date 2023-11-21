dc8efbeeb1abb3c996568dfbe20d22af868a98a9
AndreMuderhwa <safariandre66@gmail.com>
2023-11-21 16:40:31 +0100

    js and htmx

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,7 +3,8 @@ from .views import (
     recipe_list_view,
     recipe_detail_view,
     recipe_create_view,
-    recipe_update_view
+    recipe_update_view,
+    recipe_detail_hx_view
 )
 
 app_name="recipes"
@@ -12,6 +13,7 @@ urlpatterns = [
     path("",recipe_list_view,name="list"),
     path("create/",recipe_create_view, name="create"),
     path("<int:id>/edit/",recipe_update_view, name="update"),
+    path("hx/<int:id>/",recipe_detail_hx_view, name="hx-detail"),
     path("<int:id>/",recipe_detail_view, name="detail")
     
 ]
