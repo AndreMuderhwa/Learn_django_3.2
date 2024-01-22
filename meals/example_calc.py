e87bc5aba3b133c72c08fda51e8fa6735cf3da6b
AndreMuderhwa <safariandre66@gmail.com>
2024-01-22 11:19:17 +0100

    claculate meal

---
+++ meals/example_calc.py
@@ -0,0 +1,18 @@
+from django.contrib.auth import get_user_model
+from meals.models import Meal
+from recipes.models import RecipeIngredient
+
+User=get_user_model()
+j=User.objects.first()
+
+j.meal_set.all().pending() 
+queue=Meal.objects.by_user(j).pending().prefetch_related('recipe__recipeingredient')
+ids=queue.values_list("recipe__recipeingredient__id", flat=True)
+ 
+qs=RecipeIngredient.objects.filter(id=ids) 
+
+data=qs.values("name","unit","quantity_as_float")
+
+
+
+
