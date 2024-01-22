e9f8f216bccbbc60ae80b34e9ce01389c888810e
AndreMuderhwa <safariandre66@gmail.com>
2024-01-22 11:30:31 +0100

    calucation about meals

--- meals/example_calc.py
+++ meals/example_calc.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from meals.models import Meal
+from django.db.models import Sum
 from recipes.models import RecipeIngredient
 
 User=get_user_model()
@@ -9,9 +10,13 @@ j.meal_set.all().pending()
 queue=Meal.objects.by_user(j).pending().prefetch_related('recipe__recipeingredient')
 ids=queue.values_list("recipe__recipeingredient__id", flat=True)
  
-qs=RecipeIngredient.objects.filter(id=ids) 
+qs=RecipeIngredient.objects.filter(id__in=ids) 
 
-data=qs.values("name","unit","quantity_as_float")
+data=qs.values("name","unit").annotate(total=Sum("quantity_as_float"))
+
+for d in data:
+    print(d)
+
 
 
 
